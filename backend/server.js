3e690ad7338cf93b40bdc972913471ad44826a02
eliseucostaalbano <ELISEUALBANO57@GMAIL.COM>
2025-06-10 13:52:13 -0300

    iniciando o servidor

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Olá, mundo!');
+});
+
 app.listen(3000, () => {
   console.log('Servidor rodando em  http://localhost:3000');
 });
