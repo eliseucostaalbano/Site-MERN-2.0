79f5534527e332ca8fa50c5b97d4f3fb5c268f44
eliseucostaalbano <ELISEUALBANO57@GMAIL.COM>
2025-06-12 13:30:46 -0300

    adicionando o nodemon como dependência de desenvolvimento e criando as rotas

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
-  res.send('Olá, mundo!');
+  res.status(200).send('Olá, mundo do backend!');
 });
+
+app.post('/', (req, res) => {
+  res.status(201).json({ message: 'Dados recebidos com sucesso!' });
+});
+
+app.put('/', (req, res) => {
+  res.status(200).json({ message: 'Dados atualizados com sucesso!' });
+});
+
+app.delete('/', (req, res) => {
+  res.status(204).send();
+});
+
 
 app.listen(3000, () => {
   console.log('Servidor rodando em  http://localhost:3000');
